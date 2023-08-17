84f69970709b44035d0cac45a5e499da02a8c76d
uanish91 <47721887+uanish91@users.noreply.github.com>
2023-08-17 14:12:00 -0400

    Update Ask_shibe.py

--- my_env/Ask_shibe.py
+++ my_env/Ask_shibe.py
@@ -1,88 +1,70 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+from dotenv import load_dotenv
+import os
+from PyPDF2 import PdfReader
 import streamlit as st
-import langchain
-page_config = st.set_page_config(page_title="🦜🔗 Ask the Doc App")
-import tiktoken
-from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+# Load environment variables
+load_dotenv()
 
 
 
-open_api_key = st.sidebar.text_input('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')
+def process_text(text):
+    # Split the text into chunks using Langchain's CharacterTextSplitter
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    
+    # Convert the chunks of text into embeddings to form a knowledge base
+    embeddings = OpenAIEmbeddings()
+    knowledgeBase = FAISS.from_texts(chunks, embeddings)
+    
+    return knowledgeBase
 
-#Define a custom generate_response function
-def generate_response(uploaded_file, openai_api_key, query_text):
-    if uploaded_file is not None:
-        documents = [uploaded_file.read().decode()]
 
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.create_documents(documents)
-    #Select embeddings:
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    #Create a vectorstore from documents
-    db = Chroma.from_documents(texts, embeddings)
-    #Create retriever interface
-    retriever = db.as_retriever()
-    #Create QA chain
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type = 'stuff', retriever = retriever)
-    return qa.run(query_text)
+def main():
+    st.title("Chat with your PDF 💬")
+    
+    pdf = st.file_uploader('Upload your PDF Document', type='pdf')
+    
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        # Text variable will store the pdf text
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        # Create the knowledge base object
+        knowledgeBase = process_text(text)
+        
+        query = st.text_input('Ask a question to the PDF')
+        cancel_button = st.button('Cancel')
+        
+        if cancel_button:
+            st.stop()
+        
+        if query:
+            docs = knowledgeBase.similarity_search(query)
 
-# Page title
-st.title('🦜🔗 Ask the Doc App')
 
-#Add input widgets to allow users to upload text files.
-uploaded_file = st.file_uploader('Upload an article', type='txt')
-
-#Query text
-query_text= st.text_input('Enter your question', placeholder='Please provide a short summary', disabled=not uploaded_file)
-
-# Form input and query
-result = []
-with st.form('myform', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
-    if submitted and openai_api_key.startswith('sk-'):
-        with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, openai_api_key, query_text)
-            result.append(response)
-            del openai_api_key
-
-if len(result):
-    st.info(response)
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type='stuff')
+            
+            with get_openai_callback() as cost:
+                response = chain.run(input_documents=docs, question=query)
+                print(cost)
+                
+            st.write(response)
+            
+            
+if __name__ == "__main__":
+    main()
